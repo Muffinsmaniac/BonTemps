c0fffdb5a80f36ec7d14fc1a83a48fdee97d50c4
Jesper <jesper.elovsson92@gmail.com>
2025-03-17 14:03:18 +0100

    Menu now vanish after selection on mobile

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <!--Reuse-able website footer  -->
 <footer>
-        <section id="adress"><p>Köpmangatan 7<br>852 31 Sundsvall<br>Sweden</section>        
+        <section id="adress"><a href= "https://www.youtube.com/watch?v=dQw4w9WgXcQ">Köpmangatan 7<br>852 31 Sundsvall<br>Sweden </a></section>        
         <img src="pictures/logo_white.png" alt="Bon Temp Logo">
     </footer>
 <script src="scripts/index.js"></script>
--- includes/menu.php
+++ includes/menu.php
@@ -1,12 +1,12 @@
 <!-- The menu with the buttons that dynamically changes the websit -->
 <div id="navContainer">
         <button id="navButton"><i class="material-icons">menu</i></button>                       
-        <div id="dropdown">
+        <nav id="dropdown">
             <ul>
             <li id="homeButton">Hem</li>
             <li id="infoButton">Om oss</li>
             <li id="productsButton">Produkter</li>
             <li id="contactButton">Kontakt</li>
             </ul>          
-        </div>
+        </nav>
     </div>
